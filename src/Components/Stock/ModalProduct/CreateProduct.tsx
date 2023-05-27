adea80bde975a4db158f1b9b6d10e6bf95505264
virussv <teteus.30.08.07@outlook.com>
2023-05-27 06:46:53 +0000

    feat: button close modal and refactored code in stock and CreateProduct

--- src/Components/Stock/ModalProduct/CreateProduct.tsx
+++ src/Components/Stock/ModalProduct/CreateProduct.tsx
@@ -6,14 +6,14 @@ interface ICreateProductProps {
   states: {
     activeModal: boolean,
     setActiveModal: React.Dispatch<React.SetStateAction<boolean>>,
-  }
+  },
 
   //this is for edit or create Product
   buttonTexts: {
     //i need change input message on css because the type input File is so bad for style,so i create other button with ::after on css,i change the message with "content:'something'"
     buttonImgText: 'create' | 'edit',
     buttonSendFormText: string,
-  }
+  },
 }
 
 type TSizeProps = [
@@ -28,7 +28,7 @@ type stockAmount = {
   p: number,
   g: number,
   gg: number,
-}
+};
 
 const arrayProductSizes:TSizeProps = ['m','p','g','gg'];
 
@@ -55,7 +55,7 @@ const ModalProduct:React.FC<ICreateProductProps> = ({states:{activeModal,setActi
       setTimeout(() => {
         document.addEventListener('pointerdown',handleOutSideClickModal);
       }, 500);
-    }
+    };
 
     //here I avoid an eventBubble and check if the click is inside the modal or outside to close the modal and remove the handleClickoutSide event, or if the click is inside the modal do nothing
     function handleOutSideClickModal(event:Event) {
@@ -63,14 +63,18 @@ const ModalProduct:React.FC<ICreateProductProps> = ({states:{activeModal,setActi
       if(!modalRef.current?.contains(event.target as Node)) {
         document.removeEventListener('pointerdown',handleOutSideClickModal);
         setActiveModal(false);
-      }
-    }
+      };
+    };
   },[setActiveModal,activeModal]);
+
+  function handleClickExitModal() {
+    setActiveModal(false);
+  };
 
   //this is form submit
   function handleSubmit(event:SyntheticEvent) {
     event.preventDefault();
-  }
+  };
 
   //here I manipulate the image loaded inside a state
   function handleChangeFile({ target }:SyntheticEvent) {
@@ -78,28 +82,29 @@ const ModalProduct:React.FC<ICreateProductProps> = ({states:{activeModal,setActi
       if(target.files[0]) {
         setActiveLoadFile(true);
         setImg(URL.createObjectURL(target.files[0]));
-      }
-    }
-  }
+      };
+    };
+  };
 
   //file is loading
   function handleClickFile() {
     setActiveLoadFile(true);
-  }
+  };
 
   //first I check if the target.id of the element exists within my state to have a correct value and know which input will be changed, then I check if the length entered is greater than 4 in the input, if so, I delete the characters above the limit, finally I confirm if the possible id values ​​are the same as my state
   function handleChangeStock({ target }:SyntheticEvent) {
     if(target instanceof HTMLInputElement && target.id in stockAmount) {
       if(target.value.length > 4) {
         target.value = target.value.substring(0,4);
-      }
+      };
       const id = target.id as 'm' | 'p' | 'g' | 'gg';
       setStockAmount((stock) => ({...stock,[id]: Number(target.value)}));
-    }
-  }
+    };
+  };
 
   return (
     <form method='post' className={`${styles.createProduct} ${activeModal ? styles.active : ''}`} onSubmit={handleSubmit} ref={modalRef} encType='multipart-data'>
+      <button className={styles.exitModal} onClick={handleClickExitModal}><span className='material-symbols-outlined'>close</span></button>
       <div className={styles.containerImg}>
         <input type='file' id='img' accept='.jpg,.png' className={`${activeLoadFile ? styles.active : ''} ${buttonImgText === 'create' ? styles.create : styles.edit}`} onClick={handleClickFile} onBlur={() => setActiveLoadFile(false)} onChange={handleChangeFile} ref={inputFile}/>
         <p>Sua imagem ficara assim:</p>
@@ -123,13 +128,15 @@ const ModalProduct:React.FC<ICreateProductProps> = ({states:{activeModal,setActi
                 <label htmlFor={size}>{size.toUpperCase()}</label>
                 <input type='number' id={size} placeholder='0' onChange={handleChangeStock}/>
             </React.Fragment>
-          )
+          );
         })}
         <p className={styles.total}>Total:</p>
         <p className={styles.totalStock}>{Object.values(stockAmount).reduce((prevValue,currentValue) => {
           //sum the stock value
           return prevValue + currentValue;
         })}</p>
+        <p className={styles.marginTop}>Vendas:</p>
+        <input type='text' placeholder='0' className={styles.marginTop}/>
       </div>
       <button type='submit' className={styles.submit}>{buttonSendFormText}</button>
     </form>
--- src/Components/Stock/Stock.tsx
+++ src/Components/Stock/Stock.tsx
@@ -3,11 +3,10 @@ import styles from './Stock.module.css';
 import shirt from './../../Assets/images/shirt.png';
 import ModalProduct from './ModalProduct/CreateProduct';
 
-
 type TEnterModal = {
   modalCreateProduct: boolean,
   modalEditProduct: boolean,
-}
+};
 
 const Stock:React.FC = () => {
   //enterModal is to say that the animation of the modal will start
@@ -15,21 +14,32 @@ const Stock:React.FC = () => {
     modalCreateProduct: false,
     modalEditProduct: false,
   });
-  //when I say element loaded it means that the component is in the html, but it starts invisible to   make the animation, and it makes such animation with the activeModal...Product is true
+  //when I say element loaded it means that the component is in the html, but it starts invisible to make the animation, and it makes such animation with the activeModal...Product is true
   //here are the states to load the animation after the already loaded element,if I don't create these 2 types of states: entermodal and activeModal...Product there is only one dry entrance,without animation. Because if the element is loaded at the same time as the animation it does not occur.
   const [activeModalCreateProduct,setActiveModalCreateProduct] = useState<boolean>(false);
   const [activeModalEditProduct,setActiveModalEditProduct] = useState<boolean>(false);
   const mainContent = useRef<HTMLElement>(null);
+  const buttonAddRef = useRef<HTMLButtonElement | null>(null);
+  const buttonEditRef = useRef<HTMLButtonElement | null>(null);
 
+  //here I check the state of the modal to enable or disable the buttons . I disable it so that when the modal opens screen readers do not read what is behind the modal and make it impossible to double click on the modal
   useEffect(() => {
-    //for the exit with animation I put it in a timeout, with 500ms because it is the animation exit time.This time is necessary because the element will come out, but I want the animation first,as I already said
     !activeModalCreateProduct && setTimeout(() => {
       setEnterModal((modal) => ({...modal,modalCreateProduct:false}));
     },500);
-
-    !activeModalEditProduct && setTimeout(() => {
+    !activeModalEditProduct &&  setTimeout(() => {
       setEnterModal((modal) => ({...modal,modalEditProduct:false}));
     },500);
+
+    if(buttonAddRef.current && buttonEditRef.current) {
+      if(activeModalCreateProduct || activeModalEditProduct) {
+        buttonAddRef.current.disabled = true;
+        buttonEditRef.current.disabled = true;
+      } else if(!activeModalCreateProduct && !activeModalEditProduct) {
+        buttonAddRef.current.disabled = false;
+        buttonEditRef.current.disabled = false;
+      }
+    }
     
     //here I add an 'alpha' class for the background of the modal to be opaque
     const header = document.querySelector('header');
@@ -46,31 +56,21 @@ const Stock:React.FC = () => {
 
   //here I check if the click is on the icon that is in span or in button, then I add an 'eventNone' class to avoid double clicks, and finally I check which modal should be activated
   function handleClick({ target }:SyntheticEvent) {
-    //the span is the icon,but i check the button as well for accessibility scripts
+    //the span is the icon but i also check the button for screen readers
     let button:HTMLButtonElement | undefined;
-    let spanIcon: HTMLSpanElement | undefined;
     if(target instanceof HTMLSpanElement) {
-      spanIcon = target;
       button = target.parentNode as HTMLButtonElement;
     } else if(target instanceof HTMLButtonElement) {
       button = target as HTMLButtonElement;
     }
 
     if(button instanceof HTMLButtonElement) {
-      button.classList.add(`${styles.eventNone}`);
-      spanIcon && (spanIcon.classList.add(`${styles.eventNone}`));
-      setTimeout(() => {
-        button && button.classList.remove(`${styles.eventNone}`);
-        spanIcon && (spanIcon.classList.remove(`${styles.eventNone}`));
-      }, 500);
-
       //again,here the timeout callback, as I said, to first say that the element will load, and then with a timeout callback to load the animation
       if(button.id === styles.createProduct) {
         setEnterModal((modal) => ({...modal,modalCreateProduct: true}));
         setTimeout(() => {
           setActiveModalCreateProduct(true);
         });
-
       } else if(button.id === styles.editProduct) {
         setEnterModal((modal) => ({...modal,modalEditProduct: true}));
         setTimeout(() => {
@@ -85,7 +85,7 @@ const Stock:React.FC = () => {
         <main className={styles.mainStockContent} ref={mainContent}>
         <section className={styles.stock}>
           <div>
-            <button onClick={handleClick} id={styles.createProduct} ><span className="material-symbols-outlined">add</span></button>
+            <button onClick={handleClick} id={styles.createProduct} ref={buttonAddRef}><span className={`material-symbols-outlined`}>add</span></button>
             <p>Adicionar Produto</p>
           </div>
           <table className={styles.stockTable}>
@@ -100,7 +100,7 @@ const Stock:React.FC = () => {
             <tbody>
                 <tr>
                   <td>
-                    <button onClick={handleClick} id={styles.editProduct}><span className='material-symbols-outlined'>edit</span></button>
+                    <button onClick={handleClick} id={styles.editProduct} ref={buttonEditRef}><span className={`material-symbols-outlined $`}>edit</span></button>
                     <img src={shirt} alt='camiseta' className={styles.productImage}/>
                   </td>
                   <td>M P G GG</td>
